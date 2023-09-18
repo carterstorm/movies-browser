aa94b947f3e84496d9fa01f50afa357026959553
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-09-18 19:20:13 +0200

    created Navigation component

--- src/App/index.js
+++ src/App/index.js
@@ -1,7 +1,8 @@
 import { HashRouter } from "react-router-dom";
+import { Navigation } from "../features/Navigation";
 
 export const App = () => (
   <HashRouter>
-
+    <Navigation />
   </HashRouter>
 );
