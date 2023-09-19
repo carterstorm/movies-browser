9aafbe4121db0b69e4f365fe31774894cb7d4368
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-09-19 22:29:02 +0200

    changed console.error message

--- src/features/movies/MoviesPage/getPopularMovies.js
+++ src/features/movies/MoviesPage/getPopularMovies.js
@@ -2,9 +2,11 @@ import axios from "axios";
 
 export const getPopularMovies = async () => {
     try {
-        const response = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=59bb7fd84287cfdfaac50896dc742401&language=en-US&page=1");
-        console.log(response.data);
+        const response = await axios.get(
+            "https://api.themoviedb.org/3/movie/popular?api_key=59bb7fd84287cfdfaac50896dc742401&language=en-US&page=1"
+        );
+        return response.data;
     } catch (error) {
-        console.error("Something bad happened");
+        console.error(error);
     };
 };
