739816614b4c92f593d3da30f43eecf5f6359345
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-09-19 22:33:57 +0200

    added simple displaying movies for tests

--- src/features/movies/MoviesPage/index.js
+++ src/features/movies/MoviesPage/index.js
@@ -1,7 +1,30 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    fetchPopularMovies,
+    selectArePopularMoviesLoading,
+    selectPopularMovies
+} from "./moviesSlice";
+
 export const MoviesPage = () => {
+    const popularMovies = useSelector(selectPopularMovies);
+    const areLoading = useSelector(selectArePopularMoviesLoading);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchPopularMovies());
+    }, [dispatch])
+
     return (
         <>
-            Movies page
+            <p>Movies page</p>
+            <div>
+                {areLoading
+                    ? "loading"
+                    : popularMovies.map(movie => <p key={movie.id}>{movie.title}</p>)
+                }
+            </div>
         </>
+
     );
 };
