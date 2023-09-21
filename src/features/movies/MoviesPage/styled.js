4231a534a8f6a9531c80dfb253d0167c628d2a0a
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-09-21 21:31:43 +0200

    changed max-width in Main

--- src/features/movies/MoviesPage/styled.js
+++ src/features/movies/MoviesPage/styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
 export const Main = styled.main`
-    max-width: ${({ theme }) => theme.breakpoints.desktop}px;
+    max-width: 1400px;
     margin: 56px auto 0;
 `;
