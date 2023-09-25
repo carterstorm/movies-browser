97ccbddf56485f1d6dd4969c3f29450f72569715
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-09-26 01:55:05 +0200

    added background color for html element

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -4,13 +4,14 @@ import "normalize.css";
 export const GlobalStyle = createGlobalStyle`
    html {
         box-sizing: border-box;
-    }
+        background-color: ${({ theme }) => theme.colors.whisper};
+    };
 
     *, ::after, ::before {
         box-sizing: inherit;
-    }
+    };
 
     #root {
         font-family: 'Poppins', sans-serif;
-    }
+    };
 `;
