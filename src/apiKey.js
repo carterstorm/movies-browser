29f545a13c9be0965c54348ff4611dd67f00634f
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-09-25 17:22:04 +0200

    created apiKey const and set page from store

---
+++ src/apiKey.js
@@ -0,0 +1,1 @@
+export const api_key = `59bb7fd84287cfdfaac50896dc742401`;
--- src/features/movies/MoviesPage/getPopularMovies.js
+++ src/features/movies/MoviesPage/getPopularMovies.js
@@ -1,9 +1,14 @@
 import axios from "axios";
+import store from "../../../store";
+import { api_key } from "../../../apiKey";
 
 export const getPopularMovies = async () => {
+    const pageNumber = store.getState().movies.page;
+    const apiKey = api_key;
+
     try {
         const response = await axios.get(
-            "https://api.themoviedb.org/3/movie/popular?api_key=59bb7fd84287cfdfaac50896dc742401&language=en-US&page=1"
+            `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=${pageNumber}`
         );
         return response.data;
     } catch (error) {
