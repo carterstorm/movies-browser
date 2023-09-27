5ad053feebdf3363e089a683c11678036bc45826
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-09-27 22:45:10 +0200

    added options to add forward props

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { GlobalStyle } from './GlobalStyle';
-import { ThemeProvider } from 'styled-components';
+import { StyleSheetManager, ThemeProvider } from 'styled-components';
+import isPropsValid from '@emotion/is-prop-valid'
 import { theme } from "./theme";
 import store from './store';
 import { Provider } from 'react-redux';
@@ -10,11 +11,13 @@ import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
-    </Provider>
+    <StyleSheetManager shouldForwardProp={isPropsValid} disableVendorPrefixes={false}>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </Provider>
+    </StyleSheetManager>
   </React.StrictMode>
 );
