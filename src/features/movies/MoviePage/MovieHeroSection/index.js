550dd0e0a850777ca70eae15534cf7ee8ff7ca43
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-09-27 21:18:05 +0200

    styled MovieImageContainer

--- src/features/movies/MoviePage/MovieHeroSection/index.js
+++ src/features/movies/MoviePage/MovieHeroSection/index.js
@@ -1,9 +1,12 @@
-import { MovieImage, MovieImageContainer, Wrapper } from "./styled";
+import { MovieDetails, MovieImage, MovieImageContainer, Wrapper } from "./styled";
 
 export const MovieHeroSection = ({ backdrop_path }) => {
     return (
         <Wrapper>
             <MovieImageContainer>
+                <MovieDetails>
+
+                </MovieDetails>
                 <MovieImage
                     src={`https://image.tmdb.org/t/p/w1280${backdrop_path}`}
                 />
