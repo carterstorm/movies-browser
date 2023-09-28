4f56f61867142c584d94d953b4e781b2f85f7de9
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-09-29 00:49:18 +0200

    changed spaces to grid-gap

--- src/common/MovieTile/MovieInfo/styled.js
+++ src/common/MovieTile/MovieInfo/styled.js
@@ -1,13 +1,14 @@
 import styled from "styled-components";
 
 export const MovieInfoContainer = styled.div`
-    margin-bottom: 8px;
+    display: grid;
+    grid-gap: 8px;
 `;
 
 export const MovieName = styled.p`
     font-size: 22px;
     font-weight: 500;
-    margin: 0 0 8px 0;
+    margin: 0;
     line-height: 130%;
 
     @media(max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
