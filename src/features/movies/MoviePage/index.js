ba1a295aaefeb8b5e1673217e4fe13e7bca7f8f7
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-09-28 23:21:51 +0200

    added MovieTile and useEffect fechGenres

--- src/features/movies/MoviePage/index.js
+++ src/features/movies/MoviePage/index.js
@@ -2,14 +2,15 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { Checker } from "../../../common/Checker";
+import { MovieTile } from "../../../common/MovieTile";
+import { MovieHeroSection } from "./MovieHeroSection";
 import {
     fetchMovieDetails,
     selectAreMovieDetailsError,
     selectAreMovieDetailsLoading,
     selectMovieDetailsList,
 } from "../../../movieDetailsSlice";
-import { MovieHeroSection } from "./MovieHeroSection";
-import { MovieTile } from "../../../common/MovieTile";
+import { fetchGenres } from "../../../genresSlice";
 import { BigTileMovieContainer } from "./styled";
 
 export const MoviePage = () => {
@@ -31,33 +32,36 @@ export const MoviePage = () => {
         dispatch(fetchMovieDetails(id));
     }, [dispatch, id]);
 
+    useEffect(() => {
+        dispatch(fetchGenres())
+    }, [dispatch])
+
     return (
         <Checker
             areLoading={areLoading}
             areError={areError}>
             {backdrop_path &&
                 (
-                    <>
-                        <MovieHeroSection
-                            backdrop_path={backdrop_path}
-                            title={title}
-                            vote_average={vote_average}
-                            vote_count={vote_count}
-                        />
-                        <BigTileMovieContainer>
-                            <MovieTile
-                                key={id}
-                                genre_ids={genres}
-                                title={title}
-                                poster_path={poster_path}
-                                release_date={release_date}
-                                vote_average={vote_average}
-                                vote_count={vote_count}
-                            />
-                        </BigTileMovieContainer>
-
-                    </>
+                    <MovieHeroSection
+                        backdrop_path={backdrop_path}
+                        title={title}
+                        vote_average={vote_average}
+                        vote_count={vote_count}
+                    />
                 )}
+            {genres && (
+                <BigTileMovieContainer>
+                    <MovieTile
+                        key={id}
+                        genre_ids={genres && genres.map((genre) => genre.id)}
+                        title={title}
+                        poster_path={poster_path}
+                        release_date={release_date}
+                        vote_average={vote_average}
+                        vote_count={vote_count}
+                    />
+                </BigTileMovieContainer>
+            )}
         </Checker>
     );
 };
