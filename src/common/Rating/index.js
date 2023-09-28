305833339edac804b683fecff468d173dfcb21fb
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-09-28 23:19:52 +0200

    back to the first idea

--- src/common/Rating/index.js
+++ src/common/Rating/index.js
@@ -4,25 +4,55 @@ import {
     VoteAverage,
     VoteCount,
     VoteElements,
+    VoteMax,
 } from "./styled";
 
 export const Rating = ({
     vote_average,
     vote_count,
+    moviePage
 }) => {
-    return (
-        <RatingElement>
-            <VoteElements>
-                <StarElement />
-                <VoteAverage>
-                    {vote_average.toFixed(1)}
-                </VoteAverage>
-            </VoteElements>
-            <VoteCount
-                thin
+    if (moviePage) {
+        return (
+            <RatingElement
+                moviePage
             >
-                {vote_count} votes
-            </VoteCount>
-        </RatingElement>
-    )
+                <VoteElements
+                    moviePage
+                >
+                    <StarElement
+                        moviePage
+                    />
+                    <VoteAverage
+                        moviePage
+                    >
+                        {vote_average.toFixed(1)}
+                    </VoteAverage>
+                    <VoteMax>
+                        / 10
+                    </VoteMax>
+                </VoteElements>
+                <VoteCount
+                    thin
+                    moviePage
+                >
+                    {vote_count} votes
+                </VoteCount>
+            </RatingElement>
+        )
+    } else {
+        return (
+            <RatingElement>
+                <VoteElements>
+                    <StarElement />
+                    <VoteAverage>
+                        {vote_average.toFixed(1)}
+                    </VoteAverage>
+                </VoteElements>
+                <VoteCount thin>
+                    {vote_count} votes
+                </VoteCount>
+            </RatingElement>
+        )
+    }
 };
--- src/common/Rating/styled.js
+++ src/common/Rating/styled.js
@@ -1,15 +1,37 @@
 import { ReactComponent as Star } from "../../assets/svg/star.svg";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const RatingElement = styled.div`
     display: flex;
     align-items: flex-end;
     line-height: 0.9;
+
+    ${({ moviePage }) => moviePage && css`
+        flex-direction: column;
+        align-items: flex-start;
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.bigMobile}px) {
+            flex-direction: row;
+            align-items: flex-end;
+        };
+    `};
 `;
 
 export const VoteElements = styled.div`
     display: flex;
     align-items: flex-end;
+
+    ${({ moviePage }) => moviePage && css`
+        margin-bottom: 16px;
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.desktop}px) {
+            margin-bottom: 10px;
+        };
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.bigMobile}px) {
+            margin-bottom: 0;
+        };
+    `};
 `;
 
 export const StarElement = styled(Star)`
@@ -20,6 +42,21 @@ export const StarElement = styled(Star)`
         width: 16px;
         height: 16px;
     };
+
+    ${({ moviePage }) => moviePage && css`
+        width: 40px;
+        height: 40px;
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.bigMobile}px) {
+            width: 22px;
+            height: 22px;
+        };
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+            width: 16px;
+            height: 16px;
+        };
+    `};
 `;
 
 export const VoteAverage = styled.span`
@@ -30,6 +67,24 @@ export const VoteAverage = styled.span`
         font-size: 10px;
         margin-left: 8px;
     };
+
+    ${({ moviePage }) => moviePage && css`
+        font-size: 30px;
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.desktop}px) {
+            margin-left: 6px;
+        };
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.bigMobile}px) {
+            font-size: 22px;
+            flex-direction: row;
+            align-items: flex-end;
+        };
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+            font-size: 14px;
+        };
+    `};
 `;
 
 export const VoteCount = styled.span`
@@ -41,6 +96,26 @@ export const VoteCount = styled.span`
         font-size: 10px;
         margin-left: 8px;
     };
+
+    ${({ moviePage }) => moviePage && css`
+        margin: 0 0 16px 0;
+        color: ${({ theme }) => theme.colors.waterloo};
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.desktop}px) {
+            margin-bottom: 10px;
+            font-size: 12px;
+        };
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.bigMobile}px) {
+            margin-bottom: 0;
+            margin-left: 16px;
+        };
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+            font-size: 10px;
+            margin-left: 8px;
+        };
+    `};
 `;
 
 export const VoteMax = styled.span`
