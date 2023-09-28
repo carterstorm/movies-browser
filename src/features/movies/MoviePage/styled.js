db22213f459cf7a3bfaec59db0530b5f2f6124c6
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-09-29 00:50:43 +0200

    changed element to div

--- src/features/movies/MoviePage/styled.js
+++ src/features/movies/MoviePage/styled.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
-export const BigTileMovieContainer = styled.ul`
-    padding: 16px;
+export const BigTileMovieContainer = styled.div`
+    padding: 0 16px;
     margin: 0 auto;
     max-width: ${({ theme }) => theme.breakpoints.desktop}px;
     list-style: none;
