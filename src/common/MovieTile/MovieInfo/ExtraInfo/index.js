2dcac6cea688fbc859d70c6d2035d5f6eb649f73
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-09-29 18:32:13 +0200

    added spaces,condition when something doesnt exist

--- src/common/MovieTile/MovieInfo/ExtraInfo/index.js
+++ src/common/MovieTile/MovieInfo/ExtraInfo/index.js
@@ -1,15 +1,29 @@
-import { ExtraInfoContainer, InfoContainer, InfoDesctiption, InfoTitle } from "./styled";
+import {
+    ExtraInfoContainer,
+    InfoContainer,
+    InfoDesctiption,
+    InfoTitle
+} from "./styled";
 
 export const ExtraInfo = ({ original_language, runtime }) => {
     return (
         <ExtraInfoContainer>
             <InfoContainer>
-                <InfoTitle>Orginal language:</InfoTitle>
-                <InfoDesctiption upperCase>{original_language}</InfoDesctiption>
+                <InfoTitle>
+                    Orginal language:
+                </InfoTitle>
+                <InfoDesctiption
+                    upperCase>
+                    {original_language}
+                </InfoDesctiption>
             </InfoContainer>
             <InfoContainer>
-                <InfoTitle>Movie length:</InfoTitle>
-                <InfoDesctiption>{runtime} min</InfoDesctiption>
+                <InfoTitle>
+                    Movie length:
+                </InfoTitle>
+                <InfoDesctiption>
+                    {runtime} min
+                </InfoDesctiption>
             </InfoContainer>
         </ExtraInfoContainer>
     );
--- src/common/MovieTile/MovieInfo/MoreInfo/index.js
+++ src/common/MovieTile/MovieInfo/MoreInfo/index.js
@@ -1,27 +1,38 @@
-import { More, MoreInfoElement, MoreInfoElementDescription, MoreInfoElementTitle } from "./styled";
+import {
+    More,
+    MoreInfoElement,
+    MoreInfoElementDescription,
+    MoreInfoElementTitle
+} from "./styled";
 
 export const MoreInfo = ({ budget, production_countries, releaseDate }) => (
     <More>
         <MoreInfoElement>
-            <MoreInfoElementTitle>Budget:</MoreInfoElementTitle>
+            <MoreInfoElementTitle>
+                Budget:
+            </MoreInfoElementTitle>
             <MoreInfoElementDescription>
-                {budget ? `${budget} $` : 'No information'}
+                {budget ? `${budget}$` : 'No information'}
             </MoreInfoElementDescription>
         </MoreInfoElement>
         <MoreInfoElement>
-            <MoreInfoElementTitle>Production countries:</MoreInfoElementTitle>
-            {production_countries.map(({ name }) => (
+            <MoreInfoElementTitle>
+                Production countries:
+            </MoreInfoElementTitle>
+            {production_countries.length !== 0 ? production_countries.map(({ name }) => (
                 <MoreInfoElementDescription
                     key={name}
                 >
                     {name}
                 </MoreInfoElementDescription>
-            ))}
+            )) : `No information`}
         </MoreInfoElement>
         <MoreInfoElement>
-            <MoreInfoElementTitle>Release date:</MoreInfoElementTitle>
+            <MoreInfoElementTitle>
+                Release date:
+            </MoreInfoElementTitle>
             <MoreInfoElementDescription>
-                {releaseDate}
+                {releaseDate ? releaseDate : `No information`}
             </MoreInfoElementDescription>
         </MoreInfoElement>
     </More>
