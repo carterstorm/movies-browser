12a60a456f20120a90fed9cd676013ed576c3075
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-10-02 19:32:46 +0200

    set padding in MovieDetails to 0

--- src/common/MovieTile/styled.js
+++ src/common/MovieTile/styled.js
@@ -111,6 +111,7 @@ export const MovieDetails = styled.div`
 
         @media(max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
             margin-left: 12px;
+            padding: 0;
         };
     `};
 
