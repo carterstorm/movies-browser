4a175cfb18aced71eb0517483339b71f1613e343
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-10-04 00:24:48 +0200

    used reusable function in genresSaga

--- src/genresSaga.js
+++ src/genresSaga.js
@@ -1,12 +1,16 @@
 import { takeEvery, call, put, delay } from "redux-saga/effects";
-import { getGenres } from "./getGenres";
 import { fetchGenres, fetchGenresError, fetchGenresSuccess } from "./genresSlice";
+import { getApiData } from "./getApiData";
+import { api_key } from "./apiKey";
 
 function* fetchGenresHandler() {
+    const apiKey = api_key;
+
     try {
         yield delay(500);
-        const genres = yield call(getGenres);
-        yield put(fetchGenresSuccess(genres));
+        const genres = yield call(() => getApiData(
+            `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`));
+        yield put(fetchGenresSuccess(genres.genres));
     } catch (error) {
         yield put(fetchGenresError());
         yield call(alert, "Something go wrong!");
