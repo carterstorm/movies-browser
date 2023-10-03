8b6b78176b88a5210b3ee56e3ee357d1c066e24f
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-10-03 21:19:05 +0200

    created controlled component in Search element

--- src/common/Navigation/Search/index.js
+++ src/common/Navigation/Search/index.js
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import { SearchBox, SearchBoxButton, SearchBoxInput, SearchImage } from "./styled";
 
-export const Search = () => (
-    <SearchBox>
-        <SearchBoxButton>
-            <SearchImage />
-        </SearchBoxButton>
-        <SearchBoxInput
-            placeholder="Search for movies..."
-        />
-    </SearchBox>
-);
+export const Search = () => {
+    const [movieTitle, setMovieTitle] = useState("");
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <SearchBox onSubmit={onFormSubmit}>
+            <SearchBoxButton>
+                <SearchImage />
+            </SearchBoxButton>
+            <SearchBoxInput
+                value={movieTitle}
+                onChange={event => setMovieTitle(event.target.value)}
+                placeholder="Search for movies..."
+            />
+        </SearchBox>
+    )
+};
