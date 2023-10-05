e387e8c1e3fea0414ec3668614d447a20d91feae
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-10-05 23:52:37 +0200

    added noPersonImage and color to theme

--- src/theme.js
+++ src/theme.js
@@ -6,6 +6,7 @@ export const theme = {
         whisper: "#F5F5FA",
         mystic: "#E4E6F0",
         pattensBlue: '#D6E4FF',
+        silver: `#C4C4C4`,
     },
     breakpoints: {
         smallMobile: 374,
