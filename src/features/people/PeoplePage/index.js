936f65df8ded2c55122f66ea8e04d48f56f361bf
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-10-05 19:37:34 +0200

    copied code for PeoplePage from MoviesPage

--- src/features/people/PeoplePage/index.js
+++ src/features/people/PeoplePage/index.js
@@ -1,7 +1,36 @@
+import { useEffect } from "react";
+import { Checker } from "../../../common/Checker";
+import { selectAreListError, selectAreListLoading, setPage, setPath } from "../../../listSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { usePageParameter } from "../../../pageParameters";
+import { Heading } from "../../../common/Heading";
+import { Pagination } from "../../../common/Pagination";
+
 export const PeoplePage = () => {
+    const dispatch = useDispatch();
+    const areLoading = useSelector(selectAreListLoading);
+    const areError = useSelector(selectAreListError);
+    const urlPageNumber = +usePageParameter("page");
+
+    useEffect(() => {
+        dispatch(setPath("popularPeople"));
+    }, [dispatch]);
+
+    useEffect(() => {
+        dispatch(setPage(urlPageNumber < 1 || urlPageNumber > 500 ? 1 : urlPageNumber));
+    }, [dispatch, urlPageNumber]);
+
     return (
         <>
-            Peope page
+            <Checker
+                areLoading={areLoading}
+                areError={areError}
+            >
+                <Heading
+                    title="Popular people"
+                />
+                <Pagination />
+            </Checker>
         </>
     );
 };
