3b94936bd31f7718e207bd5aaaab1abe8cc8927a
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-10-07 00:55:02 +0200

    added Checker and BigTileElement to PersonPage

--- src/features/people/PersonPage/index.js
+++ src/features/people/PersonPage/index.js
@@ -1,17 +1,29 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { fetchPersonDetails } from "../../../personDetailsSlice";
+import { fetchPersonDetails, selectArePersonDetailsError, selectArePersonDetailsLoading } from "../../../personDetailsSlice";
+import { Checker } from "../../../common/Checker";
+import { BigTileElement } from "../../../common/tile/BigTileElement";
+import { BigPersonTile } from "../../../common/tile/BigTileElement/BigPersonTile";
 
 export const PersonPage = () => {
-    const { id } = useParams();
     const dispatch = useDispatch();
+    const { id } = useParams();
+    const areLoading = useSelector(selectArePersonDetailsLoading);
+    const areError = useSelector(selectArePersonDetailsError);
 
     useEffect(() => {
         dispatch(fetchPersonDetails(id));
     }, [dispatch, id]);
 
     return (
-        <>{id}</>
+        <Checker
+            areLoading={areLoading}
+            areError={areError}
+        >
+            <BigTileElement
+                children={<BigPersonTile />}
+            />
+        </Checker>
     );
 };
