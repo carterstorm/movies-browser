bda7f31a57f7b0365f07920c24c1489b53258c14
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-10-09 17:05:39 +0200

    added margin-bottom to TileContainer

--- src/common/tile/BigTileElement/styled.js
+++ src/common/tile/BigTileElement/styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const BigTileContainer = styled.div`
     padding: 0 16px;
-    margin: 0 auto;
+    margin: 0 auto 64px;
     max-width: ${({ theme }) => theme.breakpoints.desktop}px;
     list-style: none;
 `;
