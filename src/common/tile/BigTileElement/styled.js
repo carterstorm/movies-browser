0d0aca018949a64e7db1b1aef789ebb269929617
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-10-09 18:26:10 +0200

    changed styles for padding for BigTileContainer

--- src/common/tile/BigTileElement/styled.js
+++ src/common/tile/BigTileElement/styled.js
@@ -1,8 +1,11 @@
 import styled from "styled-components";
 
 export const BigTileContainer = styled.div`
-    padding: 0 16px;
     margin: 0 auto 64px;
     max-width: ${({ theme }) => theme.breakpoints.desktop}px;
     list-style: none;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.desktop}px) {
+        padding: 0 16px;
+    };
 `;
