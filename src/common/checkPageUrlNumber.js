df36faa0714cdfb934c1e1974b67c77286673013
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-10-13 21:20:01 +0200

    moved function to common function file

--- src/common/checkPageUrlNumber.js
+++
@@ -1,3 +0,0 @@
-export const checkPageUrlNumber = urlPageNumber => {
-    return urlPageNumber < 1 || urlPageNumber > 500 ? 1 : urlPageNumber;
-};
--- src/common/commonFunction.js
+++ src/common/commonFunction.js
@@ -1,3 +1,7 @@
 export const getNumberOfCast = cast => cast ? `Cast (${cast.length})` : null;
 
 export const getNumberOfCrew = crew => crew ? `Crew (${crew.length})` : null;
+
+export const checkPageUrlNumber = urlPageNumber => {
+    return urlPageNumber < 1 || urlPageNumber > 500 ? 1 : urlPageNumber;
+};
