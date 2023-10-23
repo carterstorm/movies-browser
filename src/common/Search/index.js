f820dee7bf3e30cbb512fa5c912fb8094d719a3b
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-10-23 17:35:36 +0200

    added location searchText to change input text

--- src/common/Search/index.js
+++ src/common/Search/index.js
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import { SearchBox, SearchBoxButton, SearchBoxInput, SearchImage } from "./styled";
 import { useReplacePageParameter } from "../../pageParameters";
+import { useLocation } from "react-router-dom";
 
 export const Search = () => {
+    const location = useLocation();
     const [query, setQuery] = useState("");
     const replacePageParameter = useReplacePageParameter();
+    const searchText = location.pathname.includes("movie") ? "movies" : "people";
 
     const onFormSubmit = (event) => {
         event.preventDefault();
@@ -28,7 +31,7 @@ export const Search = () => {
             <SearchBoxInput
                 value={query}
                 onChange={({ target }) => setQuery(target.value)}
-                placeholder="Search for movies..."
+                placeholder={`Search for ${searchText}...`}
             />
         </SearchBox>
     )
